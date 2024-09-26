243085992fbd9d3f304594ffbdb3f3bc4ae1c8a4
dinal <mrmihiraj@gmail.com>
2024-09-26 12:00:00 +0530

    feat: login refactored

--- client/src/pages/User/Login.js
+++ client/src/pages/User/Login.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import {Form, Button, Alert} from "react-bootstrap";
 import axios from "axios"; // Import axios
 import "../../styles/login.css";
-import BackgroundImage from "../../assets/background.png";
+import BackgroundImage from "../../assets/background.jpg";
 import Logo from "../../assets/logo.png";
 
 const Login = () => {
@@ -19,11 +19,11 @@ const Login = () => {
 
     try {
       const response = await axios.post(
-        `${process.env.REACT_APP_WEB_API}/users/login`,
-        {
-          username: inputUsername,
-          password: inputPassword,
-        }
+          `${process.env.REACT_APP_WEB_API}/users/login`,
+          {
+            username: inputUsername,
+            password: inputPassword,
+          }
       );
 
       // Assuming the API returns success with user data
@@ -44,9 +44,9 @@ const Login = () => {
     } catch (error) {
       // Corrected error handling for catch block
       if (
-        error.response &&
-        error.response.data &&
-        error.response.data.message
+          error.response &&
+          error.response.data &&
+          error.response.data.message
       ) {
         setErrorMeg(error.response.data.message);
       } else {
@@ -64,73 +64,126 @@ const Login = () => {
   };
 
   return (
-    <div
-      className="sign-in__wrapper"
-      style={{backgroundImage: `url(${BackgroundImage})`}}
-    >
-      <div className="sign-in__backdrop"></div>
-      <Form className="shadow p-4 bg-white rounded" onSubmit={handleSubmit}>
-        <img
-          className="img-thumbnail mx-auto d-block mb-2"
-          src={Logo}
-          alt="logo"
-        />
-        <div className="h4 mb-2 text-center">Sign In</div>
-        {show && (
-          <Alert
-            className="mb-2"
-            variant="danger"
-            onClose={() => setShow(false)}
-            dismissible
-          >
-            {errorMeg}
-          </Alert>
-        )}
-        <Form.Group className="mb-2" controlId="username">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type="text"
-            value={inputUsername}
-            placeholder="Username"
-            onChange={(e) => setInputUsername(e.target.value)}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-2" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={inputPassword}
-            placeholder="Password"
-            onChange={(e) => setInputPassword(e.target.value)}
-            required
-          />
-        </Form.Group>
+      <div className="login-container">
+        <div className="hero-card">
+          <h1>TROVE</h1>
+          <p>
+          Your Only E-Commerce Application!
+          </p>
+        </div>
+        <div className="login-box">
+          <main className="login">
+            <Form className="shadow p-4 rounded" onSubmit={handleSubmit}>
+              <img
+                  className="img-thumbnail mx-auto d-block mb-2"
+                  src={Logo}
+                  alt="logo"
+              />
+              <div className="h4 mb-2 text-center">Sign In</div>
+              {show && (
+                  <Alert
+                      className="mb-2"
+                      variant="danger"
+                      onClose={() => setShow(false)}
+                      dismissible
+                  >
+                    {errorMeg}
+                  </Alert>
+              )}
+              <Form.Group className="mb-2" controlId="username">
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                    type="text"
+                    value={inputUsername}
+                    placeholder="Username"
+                    onChange={(e) => setInputUsername(e.target.value)}
+                    required
+                />
+              </Form.Group>
+              <Form.Group className="mb-2" controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                    type="password"
+                    value={inputPassword}
+                    placeholder="Password"
+                    onChange={(e) => setInputPassword(e.target.value)}
+                    required
+                />
+              </Form.Group>
 
-        {!loading ? (
-          <Button className="w-100" variant="primary" type="submit">
-            Log In
-          </Button>
-        ) : (
-          <Button className="w-100" variant="primary" type="submit" disabled>
-            Logging In...
-          </Button>
-        )}
-        {/* <div className="d-grid justify-content-end">
-          <Button
-            className="text-muted px-0"
-            variant="link"
-            onClick={handlePassword}
-          >
-            Forgot password?
-          </Button>
-        </div> */}
-      </Form>
-      <div className="w-100 mb-2 position-absolute bottom-0 start-50 translate-middle-x text-white text-center">
-        Made by SLIIT-SSD | &copy;2024
+              {!loading ? (
+                  <Button className="w-100 mt-5" variant="secondary" type="submit">
+                    Log In
+                  </Button>
+              ) : (
+                  <Button className="w-100 mt-5" variant="primary" type="submit" disabled>
+                    Logging In...
+                  </Button>
+              )}
+            </Form>
+          </main>
+          <footer className="signup-footer">
+            <p><a href="#">Don't have an account?</a></p>
+          </footer>
+        </div>
       </div>
-    </div>
   );
 };
 
 export default Login;
+
+
+// <div
+//     className="sign-in__wrapper"
+//     style={{backgroundImage: `url(${BackgroundImage})`}}
+// >
+//   <div className="sign-in__backdrop"></div>
+//   <Form className="shadow p-4 bg-white rounded" onSubmit={handleSubmit}>
+//     <img
+//         className="img-thumbnail mx-auto d-block mb-2"
+//         src={Logo}
+//         alt="logo"
+//     />
+//     <div className="h4 mb-2 text-center">Sign In</div>
+//     {show && (
+//         <Alert
+//             className="mb-2"
+//             variant="danger"
+//             onClose={() => setShow(false)}
+//             dismissible
+//         >
+//           {errorMeg}
+//         </Alert>
+//     )}
+//     <Form.Group className="mb-2" controlId="username">
+//       <Form.Label>Username</Form.Label>
+//       <Form.Control
+//           type="text"
+//           value={inputUsername}
+//           placeholder="Username"
+//           onChange={(e) => setInputUsername(e.target.value)}
+//           required
+//       />
+//     </Form.Group>
+//     <Form.Group className="mb-2" controlId="password">
+//       <Form.Label>Password</Form.Label>
+//       <Form.Control
+//           type="password"
+//           value={inputPassword}
+//           placeholder="Password"
+//           onChange={(e) => setInputPassword(e.target.value)}
+//           required
+//       />
+//     </Form.Group>
+//
+//     {!loading ? (
+//         <Button className="w-100 mt-5" variant="secondary" type="submit">
+//         Log In
+//         </Button>
+//     ) : (
+//         <Button className="w-100 mt-5" variant="primary" type="submit" disabled>
+//           Logging In...
+//         </Button>
+//     )}
+//   </Form>
+// </div>
