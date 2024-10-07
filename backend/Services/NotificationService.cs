344ee30b9debc6c5dcc995525002ef8f9aa6ff80
Sayuru Bopitiya <sayururehan77@gmail.com>
2024-10-07 11:20:56 +0530

    issue fixes

--- backend/Services/NotificationService.cs
+++ backend/Services/NotificationService.cs
@@ -1,15 +1,18 @@
 using backend.DTOs;
 using backend.Interfaces;
 using backend.Models;
+
 namespace backend.Services
 {
     public class NotificationService : INotificationService
     {
         private readonly INotificationRepository _notificationRepository;
+
         public NotificationService(INotificationRepository notificationRepository)
         {
             _notificationRepository = notificationRepository;
         }
+
         public async Task AddNotification(NotificationDTO notificationDTO)
         {
             var notification = new Notification
@@ -21,6 +24,7 @@ namespace backend.Services
             };
             await _notificationRepository.AddNotification(notification);
         }
+
         public async Task<IEnumerable<Notification>> GetNotificationsByCustomerId(string customerId)
         {
             return await _notificationRepository.GetNotificationsByCustomerId(customerId);
