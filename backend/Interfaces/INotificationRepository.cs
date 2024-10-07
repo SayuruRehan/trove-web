e8781e7b4ce104c1bb6e2923853003b52fb948ed
Sayuru Bopitiya <sayururehan77@gmail.com>
2024-10-07 09:26:14 +0530

    notification interfaces

---
+++ backend/Interfaces/INotificationRepository.cs
@@ -0,0 +1,9 @@
+using backend.Models;
+namespace backend.Interfaces
+{
+    public interface INotificationRepository
+    {
+        Task AddNotification(Notification notification);
+        Task<IEnumerable<Notification>> GetNotificationsByCustomerId(string customerId);
+    }
+}
---
+++ backend/Interfaces/INotificationService.cs
@@ -0,0 +1,10 @@
+using backend.DTOs;
+using backend.Models;
+namespace backend.Interfaces
+{
+    public interface INotificationService
+    {
+        Task AddNotification(NotificationDTO notificationDTO);
+        Task<IEnumerable<Notification>> GetNotificationsByCustomerId(string customerId);
+    }
+}
